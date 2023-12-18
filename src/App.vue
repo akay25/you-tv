be7558bf02da06dc0967b2a2e5290ba3c6950845
dipen kukadiya <dipenkukadiya@gmail.com>
2023-12-18 14:34:50 +0530

    you tv video border

--- src/App.vue
+++ src/App.vue
@@ -53,13 +53,14 @@ onMounted(async () => {
     </div>
     <div class="video-page" v-else>
       <div class="video-header">
-        Title: {{ videoData.title }} <br />
+        Title: {{ videoData.title }}
+         <br />
         Uploader: {{ videoData.uploader.name }}
+
       </div>
-      <div class="video-player">
-        <!-- Video player with title overlay -->
-        <div class="video-title-overlay">{{ videoData.title }}</div>
+      <div class="video-player"> 
         <video :src="videoURLs[0]?.url" controls muted :poster="videoThumbnail" class="video-element"></video>
+        
       </div>
       <div class="sidebar">
         Chat Window will go here
@@ -68,26 +69,3 @@ onMounted(async () => {
   </div>
 </template>
 
-<style scoped>
-.video-player {
-  position: relative;
-}
-
-.video-element {
-  width: 100%;
-  height: auto;
-  z-index: 1; /* Ensure the video is above the overlay */
-}
-
-.video-title-overlay {
-  position: absolute;
-  top: 10px; /* Adjust as needed */
-  left: 10px; /* Adjust as needed */
-  background: rgba(0, 0, 0, 0.5); /* Semi-transparent black background */
-  color: white;
-  padding: 5px;
-  font-size: 16px;
-  font-weight: bold;
-  z-index: 2; /* Ensure the overlay is above the video */
-}
-</style>
